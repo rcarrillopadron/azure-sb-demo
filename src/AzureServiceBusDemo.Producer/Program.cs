0f0e66025620e60460aa85dcd5229a589d430fd3
Roberto Carrillo <email@robertocarrillo.com>
2019-12-30 18:46:27 -0600

    Structure

---
+++ src/AzureServiceBusDemo.Producer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureServiceBusDemo.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/AzureServiceBusDemo.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AzureServiceBusDemo.Shared
+{
+    public class Class1
+    {
+    }
+}
